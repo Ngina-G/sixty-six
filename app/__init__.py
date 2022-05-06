4e90d78c40f68d019ac55ec92dfc9532ed7d6cd0
Ngina <Nginagitau11@outlook.com>
2022-05-06 17:35:31 +0300

    update: adds app configurations and initializes flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,21 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from Config import config_options
 
 #Initializing Application and Initializing Flask Extensions
 
 bootstrap = Bootstrap()
 
-# def create_app()
+def create_app(config_name):
+    """Creating app configurations
+        Initializing flask extensions
+        Registering the blueprint
+        setting Config
+        Adds the views and forms
+    """
+
+    app = Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
+    bootstrap.init_app(app)
+    
