de678cb00f43f29b394ca1b7f3c5e666166ac6b6
Ngina <Nginagitau11@outlook.com>
2022-05-06 18:01:19 +0300

    update: adds a defferent name to app initialization test and the manager of the test

--- manage.py
+++ manage.py
@@ -11,4 +11,8 @@ def test():
     """Runs the unit tests
     """
     import unittest
-    tests = unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == 'main':
+    manager.run()
