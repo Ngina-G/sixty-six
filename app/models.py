3e9093d4aeadffda0874af6c3e9965d5f32d94b2
Ngina <Nginagitau11@outlook.com>
2022-05-07 16:12:17 +0300

    update: adds the database shell

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class User(db.Model):
     User class that will help in creating users
     """
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = true)
+    id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
 
     def __repr__(self):
