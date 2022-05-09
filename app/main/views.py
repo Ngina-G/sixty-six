1db5ff9ab10e4f17d45c8aea4955852262d10fb5
Ngina <Nginagitau11@outlook.com>
2022-05-09 20:06:46 +0300

    update: adds index function

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,19 @@
 from flask import render_template
 from flask_login import login_required
 from . import main
+from ..requests import get_all_pitches, get_pitch
+from ..models import Comment
 
 @main.route('/')
 def index():
     """
     View root page function that returns the index page and its data
     """
-    return render_template('index.html')
+    get_all_pitches = get_all_pitches()
+    title = 'Home'
+    return render_template('index.html', title=title, pitches=get_all_pitches)
+
+
 
 # @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 # @login_required
