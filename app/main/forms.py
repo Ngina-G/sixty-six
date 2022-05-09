f097fc8b79ba26c67d2d25231f84e35d953be69c
Ngina <Nginagitau11@outlook.com>
2022-05-09 20:25:17 +0300

    updae:adds pitch form to views

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,8 +2,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import ValidationError, InputRequired
 
-# class ReviewForm(FlaskForm):
+class PitchForm(FlaskForm):
 
-#     title = StringField('Review title',validators=[InputRequired()])
-#     review = TextAreaField('Movie review', validators=[InputRequired()])
-#     submit = SubmitField('Submit')
+    category = StringField('Add Category (business, love, comedy, insiprational)',validators=[InputRequired()])
+    pitch = TextAreaField('Pitch', validators=[InputRequired()])
+    pitch_author = StringField('Your Name', validators=[InputRequired()])
+    submit_pitch = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+
+    content = TextAreaField('Pitch', validators=[InputRequired()])
+    author = StringField('Your Name', validators=[InputRequired()])
+    submit_comment = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -3,17 +3,28 @@ from flask_login import login_required
 from . import main
 from ..requests import get_all_pitches, get_pitch
 from ..models import Comment
+from .forms import PitchForm, CommentForm
 
 @main.route('/')
 def index():
     """
     View root page function that returns the index page and its data
     """
-    get_all_pitches = get_all_pitches()
+    get_all_pitches = Pitches.get_all_pitches()
     title = 'Home'
     return render_template('index.html', title=title, pitches=get_all_pitches)
 
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        category = form.category.data
+        pitch = form.pitch.data
+        author = form.pitch_author.data
 
+        new_pitch = Pitch(id,category,pitch,pitch_author)
+        new_pitch.save_pitch()
+
+        return render_template('index.html',category=category,pitch=pitch,author=author)
 
 # @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 # @login_required
