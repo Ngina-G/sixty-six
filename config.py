8a9ff943a6ffbf881bdaa512e2808a205d33ed2b
Ngina <Nginagitau11@outlook.com>
2022-05-15 18:40:35 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -29,7 +29,9 @@ class ProdConfig(Config):
     Args:
         Config (The parent configuration class): with General production configuration settings
     """
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Postgres@localhost/sixtysixsec'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # .replace("://", "ql://", 1)
     DEBUG =True
     WTF_CSRF_SECRET_KEY="a csrf secret key" 
 
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 export MAIL_USERNAME='gitaungina@gmail.com'
 export MAIL_PASSWORD='oncology'
 export SECRET_KEY='secretsixtysix123xisytxisterces'
+# export DATABASE_URL=postgres://$(ngina)
 python3 manage.py server
