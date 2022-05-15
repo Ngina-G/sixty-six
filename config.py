6ddf52fe76abefbe3a6dc1e8af7a06e8021bc857
Ngina <Nginagitau11@outlook.com>
2022-05-15 17:59:26 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ class Config:
     # SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Postgres@localhost:5432/sixtysixsec'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # UPLOADED_PHOTOS_DEST ='app/static/photos'
-    UPLOADED_PHOTOS_DEST ='static/img'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    # UPLOADED_PHOTOS_DEST ='static/img'
 
 #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -29,8 +29,11 @@ class ProdConfig(Config):
     Args:
         Config (The parent configuration class): with General production configuration settings
     """
-    pass
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
+    DEBUG =True
+    WTF_CSRF_SECRET_KEY="a csrf secret key" 
+
+
 
 class Testconfig(Config):
     """Test configuration child class
@@ -47,11 +50,11 @@ class DevConfig(Config):
     Args:
         Config (the parent configuration class): with General configuration settngs
     """
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Postgres@localhost:5432/sixtysixsec'
+    pass
 
-    WTF_CSRF_SECRET_KEY="a csrf secret key" 
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Postgres@localhost:5432/sixtysixsec'
 
-    DEBUG =True
+
 
 config_options = {
     'development':DevConfig,
